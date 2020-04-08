41611a020b24375a4c86169c4e893ad89b321b19
AnicetR <anicet.reglat@gmail.com>
2020-04-08 17:56:08 +0200

    updateEvery

--- src/api/index.js
+++ src/api/index.js
@@ -1,28 +1,35 @@
 import config from "../config";
 import cityState from "../states/cityState";
-import weatherInfosState from '../states/weatherInfosState';
+import weatherInfosState from "../states/weatherInfosState";
 
 async function apiCall(city, endpoint) {
-    const cityReq = `q=${city}`;
-    const apiKeyReq = `appid=${config.apiKey}`;
+  const cityReq = `q=${city}`;
+  const apiKeyReq = `appid=${config.apiKey}`;
 
-    const response = await fetch(`${config.apiUrl}${endpoint}?${cityReq}&${apiKeyReq}`);
-    const json = await response.json();
+  const response = await fetch(
+    `${config.apiUrl}${endpoint}?${cityReq}&${apiKeyReq}`
+  );
+  const json = await response.json();
 
-    return json;
+  return json;
 }
 
-export async function getCurrentWeather(){
-    const city = new cityState().getData().data ?? config.defaultCity;
-    const currentWeatherData = await apiCall(city, 'weather');
-    const dailyForecast = await apiCall(city, 'forecast');
+export async function getCurrentWeather() {
+  const city = new cityState().getData().data ?? config.defaultCity;
+  const weatherState = new weatherInfosState();
 
-    const weatherState = new weatherInfosState();
+  if (
+    city !== weatherState.data.cityName ||
+    weatherState.lastUpdate + config.updateEvery * 60 * 60 * 1000 < Date.now()
+  ) {
+    const currentWeatherData = await apiCall(city, "weather");
+    const dailyForecast = await apiCall(city, "forecast");
 
     weatherState.setData(city, {
-        current: currentWeatherData,
-        dailyForecast : dailyForecast
-    })
+      current: currentWeatherData,
+      dailyForecast: dailyForecast,
+    });
+  }
 
-    return weatherState.getData();
+  return weatherState.getData();
 }
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
     apiUrl : 'https://api.openweathermap.org/data/2.5/',
     apiKey : 'd3302d5cd8802f9753d113b9af2e24e2',
-    defaultCity: 'toulouse'
+    defaultCity: 'toulouse',
+    updateEvery: 2 //in hours
 }
