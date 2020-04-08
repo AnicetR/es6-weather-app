8b287345cc0bd6c063fa37d1fc2190cc4c10c0da
AnicetR <anicet.reglat@gmail.com>
2020-04-08 19:38:27 +0200

    dd : get data if null in storage

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import eventBus from "eventBus";
 import { getCurrentWeather } from "./api";
 import config from "config";
 import forecastView from "./views/forecast";
+import weatherInfosState from "./states/weatherInfosState";
 
 const main = async () => {
+  if(new weatherInfosState().getData().data === null){
+    getCurrentWeather()
+  }
   //Automatic refresh every two hours
   setTimeout(() => getCurrentWeather(), config.updateEvery * 60 * 60 * 1000);
   //Automatic data refresh if city changes
